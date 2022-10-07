dd341a259a5e48ad7559313435c749610da8fa5f
Ashok Jeevan <ashokjeevan@gmail.com>
2022-10-07 08:09:00 -0400

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /* cloud {
+    organization = "home-terraformcloud"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  } */
 
   required_providers {
     aws = {
