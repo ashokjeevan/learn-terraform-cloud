517f276b0db3053c8a55579114f4c1074def5a97
Ashok Jeevan <ashokjeevan@gmail.com>
2022-10-13 13:24:42 -0400

    using the map for testing

--- main.tf
+++ main.tf
@@ -2,50 +2,47 @@ provider "aws" {
   region = var.region
 }
 
-# Fetch AMIs from account
-data "aws_ami_ids" "windows_ami" {
+# Fetch AMIs from account - this works
+// data "aws_ami_ids" "windows_ami" {
+//   owners = ["self"]
+//   # sort_ascending = true
+
+//   // for_each = var.ami_names
+
+//   count = length(var.ami_names)
+
+//   filter {
+//     name = "name"
+//     values = [var.ami_names[count.index]]
+//   }
+// }
+
+// output "windows_ami_list_result" {
+//   value = data.aws_ami_ids.windows_ami[*].ids
+// }
+//Works till here
+
+data "aws_ami_ids" "ami_ids" {
   owners = ["self"]
-  # sort_ascending = true
 
-  // for_each = var.ami_names
-
-  count = length(var.ami_names)
+  for_each = var.ami_names_map
 
   filter {
     name = "name"
-    values = [var.ami_names[count.index]]
+    values = [each.value]
   }
 }
 
-// output "windows_ami_count_result" {
-//   value = "${length(data.aws_ami_ids.windows_ami[each.key].ids)}"
-// }
-
-output "windows_ami_list_result" {
-  value = data.aws_ami_ids.windows_ami[*].ids
+output "ami_map_results" {
+  value = data.aws_ami_ids.ami_ids.[*].ids
 }
 
-// data "aws_ami" "ubuntu" {
-//   most_recent = true
+#SSM Parameter Store creation
+// resource "aws_ssm_paramter" "ssm_parameters_ami_ids" {
 
-//   filter {
-//     name   = "name"
-//     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-//   }
 
-//   filter {
-//     name   = "virtualization-type"
-//     values = ["hvm"]
-//   }
-
-//   owners = ["099720109477"] # Canonical
+//   name = ""
+//   type = "String"
+//   value = ""
 // }
 
-// resource "aws_instance" "ubuntu" {
-//   ami           = data.aws_ami.ubuntu.id
-//   instance_type = var.instance_type
-
-//   tags = {
-//     Name = var.instance_name
-//   }
-// }
--- variables.tf
+++ variables.tf
@@ -9,6 +9,21 @@ variable "ami_names" {
   default = ["linux*", "windowsv*"]
 }
 
+variable "ami_ssm_names" {
+  description = "Names to be entered in the SSM Parameter Store"
+  type = list(string)
+  default = ["AMZN2","Windows2016"]
+}
+
+variable "ami_names_map" {
+  description = "AMI names in a map structure"
+  type = "map"
+  default = {
+    "AMZN2" = "linux*",
+    "Windows2016" = "windowsv*"
+  }
+}
+
 // variable "instance_type" {
 //   description = "Type of EC2 instance to provision"
 //   default     = "t2.micro"
